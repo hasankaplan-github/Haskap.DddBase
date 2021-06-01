4b4a48386e2230605574a4902b4549e9dfe100e6
Hasan Kaplan <hasan.kaplan@atilim.edu.tr>
2021-06-01 15:30:02 +0300

    BaseEntityTypeConfiguration eklendi.

---
+++ Haskap.DddBase.Infrastructure.DataAccess.EfCoreEntityTypeConfigurations/BaseEntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Haskap.DddBase.Domain.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Haskap.DddBase.Infrastructure.DataAccess.EfCoreEntityTypeConfigurations
+{
+    public class BaseEntityTypeConfiguration<TEntity, TId> : IEntityTypeConfiguration<TEntity>
+        where TEntity : Entity<TId>
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            if (typeof(ISoftDeletable).IsAssignableFrom(typeof(TEntity)))
+            {
+                builder.HasQueryFilter(x => (x as ISoftDeletable).IsDeleted == false);
+            }
+        }
+    }
+}
