85baf318b806ac7266ba1a5679819694f152c367
Hasan Kaplan <hasan.kaplan@atilim.edu.tr>
2021-06-01 14:33:18 +0300

    Utilities içine CustomVoid ve PagedList eklendi.

---
+++ Utilities/CustomVoid.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Utilities
+{
+    [StructLayout(LayoutKind.Sequential, Size = 0)]
+    public struct CustomVoid { }
+}
