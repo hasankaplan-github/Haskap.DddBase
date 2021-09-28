5e4c197b4b9a3bf1f03e2866de2d1d62141d0af4
Hasan Kaplan <hasan.kaplan@atilim.edu.tr>
2021-09-28 16:57:48 +0300

    Rename

--- Haskap.DddBase.Domain.Core/MediatorWrapper.cs
+++ Haskap.DddBase.Domain.Core/MediatorWrapper.cs
@@ -6,17 +6,17 @@ using MediatR;
 public static class MediatorWrapper
 {
     [ThreadStatic] // ensure separate func per thread to support parallel invocation
-    public static Func<IMediator> Mediator;
+    public static Func<IMediator> MediatorFunc;
     public static async Task Publish<TEvent>(TEvent @event, CancellationToken cancellationToken = default) 
         where TEvent : INotification
     {
-        var mediator = Mediator.Invoke();
+        var mediator = MediatorFunc.Invoke();
         await mediator.Publish<TEvent>(@event, cancellationToken);
     }
 
     public static async Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default) 
     {
-        var mediator = Mediator.Invoke();
+        var mediator = MediatorFunc.Invoke();
         return await mediator.Send<TResponse>(request, cancellationToken);
     }
 }
