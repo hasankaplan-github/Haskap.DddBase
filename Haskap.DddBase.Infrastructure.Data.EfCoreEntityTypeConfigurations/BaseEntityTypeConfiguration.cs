0edf4b800939ee2178f756fa140a7ee396b989b4
Hasan Kaplan <haskap84@hotmail.com>
2022-04-03 09:38:25 +0300

    Multitenancy için eksiklikler giderilmeye çalışılıyor.

--- Haskap.DddBase.Infrastructure.Data.EfCoreEntityTypeConfigurations/BaseEntityTypeConfiguration.cs
+++ Haskap.DddBase.Infrastructure.Data.EfCoreEntityTypeConfigurations/BaseEntityTypeConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using Haskap.DddBase.Domain.Core;
+using Haskap.DddBase.Infrastructure.Providers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -11,10 +12,27 @@ namespace Haskap.DddBase.Infrastructure.Data.EfCoreEntityTypeConfigurations
     {
         public virtual void Configure(EntityTypeBuilder<TEntity> builder)
         {
+            var isSoftDeletable = false;
             if (typeof(ISoftDeletable).IsAssignableFrom(typeof(TEntity)))
             {
+                isSoftDeletable = true;
                 builder.Property(x => (x as ISoftDeletable).IsDeleted).IsRequired();
-                builder.HasQueryFilter(x => (x as ISoftDeletable).IsDeleted == false);
+                //builder.HasQueryFilter(x => (x as ISoftDeletable).IsDeleted == false);
+            }
+
+            var hasMultiTenancy = false;
+            if (typeof(IMultiTenant).IsAssignableFrom(typeof(TEntity)))
+            {
+                hasMultiTenancy = true;
+            }
+
+            if (isSoftDeletable || hasMultiTenancy)
+            {
+                builder.HasQueryFilter(x =>
+                    (!isSoftDeletable || (x as ISoftDeletable).IsDeleted == false) &&
+                    (!hasMultiTenancy || !CurrentTenantProvider.MultiTenancyIsEnabled || (x as IMultiTenant).TenantId == CurrentTenantProvider.CurrentTenant.Id));
+                // CurrentTenant middleware içinde set edilmesi gerekiyor.
+                // https://github.com/hikalkan/presentations/blob/master/2018-04-06-Multi-Tenancy/src/MultiTenancyDraft/Infrastructure/MultiTenancyMiddleware.cs
             }
         }
     }
