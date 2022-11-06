1ac0e778335645cf394b25fc52ad4da303a748fd
Hasan Kaplan <haskap84@hotmail.com>
2022-11-06 22:36:09 +0300

    feature: Provider' lar için DI extension eklendi.

---
+++ Haskap.DddBase.Infra.Providers/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Haskap.DddBase.Domain.Providers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haskap.DddBase.Infra.Providers;
+public static class ServiceCollectionExtensions
+{
+    public static void AddProviders<TUserId>(this IServiceCollection services)
+    {
+        //services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        //services.AddSingleton<IJwtProvider, JwtProvider>();
+        services.AddScoped<ICurrentUserIdProvider<TUserId>, CurrentUserIdProvider<TUserId>>();
+        services.AddScoped<IVisitIdProvider, VisitIdProvider>();
+        services.AddScoped<ICurrentTenantProvider, CurrentTenantProvider>();
+    }
+}
