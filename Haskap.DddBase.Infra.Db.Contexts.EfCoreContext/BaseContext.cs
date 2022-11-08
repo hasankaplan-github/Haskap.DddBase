f1e68aa728079d1963b241533a37699f6dfd613c
Hasan Kaplan <haskap84@hotmail.com>
2022-11-08 17:39:17 +0300

    MultiTenancy için çalışmalar.

---
+++ Haskap.DddBase.Infra.Db.Contexts.EfCoreContext/BaseContext.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using Haskap.DddBase.Domain;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Linq.Expressions;
+using Haskap.DddBase.Domain.Providers;
+
+namespace Haskap.DddBase.Infra.Db.Contexts.EfCoreContext;
+
+public class BaseContext : DbContext
+{
+    protected ICurrentTenantProvider _currentTenantProvider;
+    private Guid? _currentTenantId => _currentTenantProvider.CurrentTenantId;
+    private bool _multiTenancyIsEnabled => _currentTenantProvider.MultiTenancyIsEnabled;
+
+    //public BaseContext(
+    //    DbContextOptions<BaseContext> options,
+    //    ICurrentTenantProvider currentTenantProvider)
+    //    : base(options)
+    //{
+    //    _currentTenantProvider = currentTenantProvider;
+    //}
+
+    protected BaseContext(
+        DbContextOptions options, 
+        ICurrentTenantProvider currentTenantProvider)
+        : base(options)
+    {
+        _currentTenantProvider = currentTenantProvider;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //TrySetDatabaseProvider(modelBuilder);
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            ConfigureBasePropertiesMethodInfo
+                .MakeGenericMethod(entityType.ClrType)
+                .Invoke(this, new object[] { modelBuilder, entityType });
+        }
+    }
+
+
+
+    private static readonly MethodInfo ConfigureBasePropertiesMethodInfo
+       = typeof(BaseContext)
+           .GetMethod(
+               nameof(ConfigureBaseProperties),
+               BindingFlags.Instance | BindingFlags.NonPublic
+           );
+
+    
+    protected virtual void ConfigureBaseProperties<TEntity>(ModelBuilder modelBuilder, IMutableEntityType mutableEntityType)
+        where TEntity : class
+    {
+        if (mutableEntityType.IsOwned())
+        {
+            return;
+        }
+
+        //if (!typeof(IEntity).IsAssignableFrom(typeof(TEntity)))
+        //{
+        //    return;
+        //}
+
+        //modelBuilder.Entity<TEntity>().ConfigureByConvention();
+
+        ConfigureGlobalFilters<TEntity>(modelBuilder, mutableEntityType);
+    }
+
+    protected virtual void ConfigureGlobalFilters<TEntity>(ModelBuilder modelBuilder, IMutableEntityType mutableEntityType)
+        where TEntity : class
+    {
+        if (mutableEntityType.BaseType == null && ShouldFilterEntity<TEntity>(mutableEntityType))
+        {
+            var filterExpression = CreateFilterExpression<TEntity>();
+            if (filterExpression != null)
+            {
+                modelBuilder.Entity<TEntity>().HasQueryFilter(filterExpression);
+            }
+        }
+    }
+
+    private bool _isSoftDeletable = false;
+    private bool _hasMultiTenant = false;
+
+    protected virtual bool ShouldFilterEntity<TEntity>(IMutableEntityType mutableEntityType)
+    {
+        _isSoftDeletable = false;
+        _hasMultiTenant = false;
+
+        if (typeof(ISoftDeletable).IsAssignableFrom(typeof(TEntity)))
+        {
+            _isSoftDeletable = true;
+        }
+
+        if (typeof(IHasMultiTenant).IsAssignableFrom(typeof(TEntity)))
+        {
+            _hasMultiTenant = true;
+        }
+
+        if (_isSoftDeletable || _hasMultiTenant)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected virtual Expression<Func<TEntity, bool>>? CreateFilterExpression<TEntity>()
+    {
+        return (x => 
+        ((!_isSoftDeletable || (x as ISoftDeletable).IsDeleted == false) &&
+         (!_hasMultiTenant || !_multiTenancyIsEnabled || (x as IHasMultiTenant).TenantId == _currentTenantId)));
+    }
+
+}
--- Haskap.DddBase.Infra.Db.Contexts.NpgsqlDbContext/BaseEfCoreNpgsqlDbContext.cs
+++ Haskap.DddBase.Infra.Db.Contexts.NpgsqlDbContext/BaseEfCoreNpgsqlDbContext.cs
@@ -5,42 +5,29 @@ using System.Linq;
 using Haskap.DddBase.Domain;
 using System.Threading.Tasks;
 using System.Threading;
+using Haskap.DddBase.Infra.Db.Contexts.EfCoreContext;
+using Haskap.DddBase.Domain.Providers;
 
 namespace Haskap.DddBase.Infra.Db.Contexts.NpgsqlDbContext;
 
-public class BaseEfCoreNpgsqlDbContext : DbContext
+public class BaseEfCoreNpgsqlDbContext : BaseContext
 {
-    ///public DbSet<AuditHistoryLog> AuditHistoryLog { get; set; }
+    //public BaseEfCoreNpgsqlDbContext(
+    //    DbContextOptions<BaseEfCoreNpgsqlDbContext> options,
+    //    ICurrentTenantProvider currentTenantProvider)
+    //    : base(options, currentTenantProvider)
+    //{
+    //}
 
-    public BaseEfCoreNpgsqlDbContext(DbContextOptions<BaseEfCoreNpgsqlDbContext> options) : base(options)
-    {
-    }
-
-    protected BaseEfCoreNpgsqlDbContext(DbContextOptions options)
-    : base(options)
+    protected BaseEfCoreNpgsqlDbContext(
+        DbContextOptions options, 
+        ICurrentTenantProvider currentTenantProvider)
+        : base(options, currentTenantProvider)
     {
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-
-        //foreach (var entityType in builder.Model.GetEntityTypes())
-        //{
-        //    entityType.SetTableName(entityType.DisplayName().ToSnakeCase(CaseOption.LowerCase));
-
-        //    foreach (var property in entityType.GetProperties())
-        //        property.SetColumnName(property.Name.ToSnakeCase(CaseOption.LowerCase));
-
-        //    //foreach (var key in entityType.GetKeys())
-        //    //    key.SetName(key.GetName().ToSnakeCase(CaseOption.LowerCase));
-
-        //    //foreach (var foreignKey in entityType.GetForeignKeys())
-        //    //    foreignKey.PrincipalKey.SetName(foreignKey.PrincipalKey.GetName().ToSnakeCase(CaseOption.LowerCase));
-        //    ////foreignKey.SetConstraintName(foreignKey.GetConstraintName().ToSnakeCase(CaseOption.LowerCase));
-
-        //    //foreach (var index in entityType.GetIndexes())
-        //    //    index.SetName(index.GetName().ToSnakeCase(CaseOption.LowerCase));
-        //}
     }
 }
