6c248164f0a15f7dd2fff02b22dc7cd47c5c3334
Hasan Kaplan <hasankaplan.kariyer@gmail.com>
2023-10-31 22:38:31 +0300

    Domain içinde IUnitOfWork oluşturuldu. İçinde DbSet ler hariç olan şeyleri barındıracak.

---
+++ Haskap.DddBase.Domain/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haskap.DddBase.Domain;
+public interface IUnitOfWork
+{
+    DatabaseFacade Database {  get; }
+
+    int SaveChanges();
+
+    int SaveChanges(bool acceptAllChangesOnSuccess);
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+
+    Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken));
+}
