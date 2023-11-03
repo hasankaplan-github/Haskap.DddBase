83910ba0ae6a4546ecdf1a6cc040e56b26f992be
Hasan Kaplan <hasankaplan.kariyer@gmail.com>
2023-11-03 09:57:16 +0300

    Constlar düzenlendi.

--- Haskap.DddBase.Domain.Shared/Consts.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Haskap.DddBase.Domain.Shared;
-public static class Consts
-{
-    public static class LocalDateTimeProviderConsts
-    {
-        public const string UserSystemTimeZoneIdClaimKey = "UserSystemTimeZoneId";
-    }
-}
