eaac69c0a9378511d02f205868bbda77b9f6de2a
Hasan Kaplan <hasankaplan.kariyer@gmail.com>
2023-12-14 17:28:48 +0300

    Current Tenant Provider a IsHost eklendi.

--- Haskap.DddBase.Domain/Providers/ICurrentTenantProvider.cs
+++ Haskap.DddBase.Domain/Providers/ICurrentTenantProvider.cs
@@ -11,6 +11,7 @@ public interface ICurrentTenantProvider
     // https://github.com/hikalkan/presentations/blob/master/2018-04-06-Multi-Tenancy/src/MultiTenancyDraft/Infrastructure/MultiTenancyMiddleware.cs
     Guid? CurrentTenantId { get; }
     
+    bool IsHost { get; }
 
     IDisposable ChangeCurrentTenant(Guid? newCurrentTenantId);
     
--- Haskap.DddBase.Infra/Providers/CurrentTenantProvider.cs
+++ Haskap.DddBase.Infra/Providers/CurrentTenantProvider.cs
@@ -3,6 +3,7 @@ using Haskap.DddBase.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace Haskap.DddBase.Infra.Providers;
 public class CurrentTenantProvider : ICurrentTenantProvider
 {
     public Guid? CurrentTenantId { get; private set; } = null;
+
+    public bool IsHost => CurrentTenantId is null;
 
     public IDisposable ChangeCurrentTenant(Guid? newCurrentTenantId)
     {
