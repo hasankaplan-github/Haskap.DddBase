c8e2f200c2f30188f3703fdf47956a03c0a9f8a8
Hasan Kaplan <hasankaplan.kariyer@gmail.com>
2024-06-15 21:05:24 +0300

    update: Localization uygulanıyor.

--- Haskap.DddBase.Domain/TenantAggregate/Exceptions/DuplicateTenantNameException.cs
+++ Haskap.DddBase.Domain/TenantAggregate/Exceptions/DuplicateTenantNameException.cs
@@ -5,13 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.Resources;
+using Haskap.DddBase.Domain.Shared.Resources;
 
 namespace Haskap.DddBase.Domain.TenantAggregate.Exceptions;
 public class DuplicateTenantNameException : DomainException
 {
     public DuplicateTenantNameException()
-        : base("Bu isim kayıtlı, farklı bir isim deneyin!", HttpStatusCode.BadRequest)
+        : base(ExceptionMessages.DuplicateTenantNameException, HttpStatusCode.BadRequest)
     {
-
+        //"Bu isim kayıtlı, farklı bir isim deneyin!"
     }
 }
