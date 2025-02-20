62d2bd6482d4fb74b3162db503b971f70b4f3412
Hasan Kaplan <hasankaplan.kariyer@gmail.com>
2025-02-20 12:44:22 +0300

    update: ModuleManagement için güncelleme yapıldı.

--- Haskap.DddBase.Utilities/Module/ModuleExtensions.cs
+++ Haskap.DddBase.Utilities/Module/ModuleExtensions.cs
@@ -9,10 +9,35 @@ using System.Threading.Tasks;
 namespace Haskap.DddBase.Utilities.Module;
 public static class ModuleExtensions
 {
-    public static IServiceCollection AddModule<T>(this IServiceCollection services, IConfiguration configuration, string connectionStringName, string? migrationAssembly) 
-        where T : class, IModule, new()
+    public static IServiceCollection AddModule<TModule>(this IServiceCollection services, IConfiguration configuration, string connectionStringName, string? migrationAssembly) 
+        where TModule : class, IModule, new()
     {
-        var module = new T();
+        if (!IsModuleEnabled<TModule>(configuration))
+        {
+            return services;
+        }
+
+        var module = new TModule();
+        return module.RegisterModule(services, configuration, connectionStringName, migrationAssembly);
+    }
+
+    public static bool IsModuleEnabled<TModule>(IConfiguration configuration)
+        where TModule : class, IModule
+    {
+        var exists = bool.TryParse(configuration[$"Modules:IsEnabled:{typeof(TModule).Name}"], out var isEnabled);
+
+        if (exists && isEnabled)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static IServiceCollection AddCoreModule<TModule>(this IServiceCollection services, IConfiguration configuration, string connectionStringName, string? migrationAssembly)
+        where TModule : class, IModule, new()
+    {
+        var module = new TModule();
         return module.RegisterModule(services, configuration, connectionStringName, migrationAssembly);
     }
 }
