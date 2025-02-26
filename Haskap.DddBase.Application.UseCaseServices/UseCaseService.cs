8da0539e37457ac9db08a2fa20e2a7c8d70c190a
Hasan Kaplan <hasankaplan.kariyer@gmail.com>
2025-02-26 14:04:17 +0300

    ..

--- Haskap.DddBase.Application.UseCaseServices/UseCaseService.cs
+++ Haskap.DddBase.Application.UseCaseServices/UseCaseService.cs
@@ -1,9 +1,8 @@
 ﻿using Haskap.DddBase.Application.Contracts;
 using System;
 
-namespace Haskap.DddBase.Application.UseCaseServices
+namespace Haskap.DddBase.Application.UseCaseServices;
+
+public class UseCaseService : IUseCaseService
 {
-    public class UseCaseService : IUseCaseService
-    {
-    }
 }
