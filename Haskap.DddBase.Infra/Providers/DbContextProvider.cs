8fc549f309e08a06fac8a5d2f7c1b356a5fe1f25
Hasan Kaplan <hasankaplan.kariyer@gmail.com>
2025-11-02 11:07:29 +0300

    update: DbContextProvider için finalizer yerine disposable kullanıldı.

--- Haskap.DddBase.Infra/Providers/DbContextProvider.cs
+++ Haskap.DddBase.Infra/Providers/DbContextProvider.cs
@@ -4,21 +4,13 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Haskap.DddBase.Infra.Providers;
 
-public class DbContextProvider : IDbContextProvider
+public class DbContextProvider : IDbContextProvider, IDisposable
 {
     private readonly IServiceScopeFactory _serviceScopeFactory;
     private readonly ICurrentTenantProvider _currentTenantProvider;
     private readonly IGlobalQueryFilterGenericProvider _globalQueryFilterGenericProvider;
     private readonly Stack<IServiceScope> _scopes = new();
-
-    ~DbContextProvider()
-    {
-        while (_scopes.Count > 0)
-        {
-            var scope = _scopes.Pop();
-            scope.Dispose();
-        }
-    }
+    private bool _disposedValue;
 
     public DbContextProvider(
         ICurrentTenantProvider currentTenantProvider,
@@ -45,5 +37,39 @@ public class DbContextProvider : IDbContextProvider
         }
 
         return scope.ServiceProvider.GetRequiredService<TDbContext>();
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+                while (_scopes.Count > 0)
+                {
+                    var scope = _scopes.Pop();
+                    scope.Dispose();
+                }
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            _disposedValue = true;
+        }
+    }
+
+    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    // ~DbContextProvider()
+    // {
+    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    //     Dispose(disposing: false);
+    // }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 }
