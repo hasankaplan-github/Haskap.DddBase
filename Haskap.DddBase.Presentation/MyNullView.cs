270329ee473b07ee6cfaeae80dd1c21b072dda86
Hasan Kaplan <hasankaplan.kariyer@gmail.com>
2026-07-04 00:21:12 +0300

    update: MyNullView class'ı eklendi.

---
+++ Haskap.DddBase.Presentation/MyNullView.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace Haskap.DddBase.Presentation;
+
+public class MyNullView : IView
+{
+    public string Path => string.Empty;
+    public Task RenderAsync(ViewContext context) => Task.CompletedTask;
+}
